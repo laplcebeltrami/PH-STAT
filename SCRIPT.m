880e8cdce4af27043cf3a18c45ef64b14ea0d319
laplcebeltrami <68470151+laplcebeltrami@users.noreply.github.com>
2023-04-04 01:28:26 -0500

    Add files via upload

--- SCRIPT.m
+++ SCRIPT.m
@@ -55,49 +55,54 @@ PH_PD_display(pairs)
 %X is the matrix of size p x d of coordinates
 p=50; d=3;
 X = rand(p, d);
-%S= PH_rips(X, 3, 0)
-%PH_rips_display(X,S)
-%title('Uniformly distributed points in [0,1]^3')
 
 %Compute the Rips complex up to 3-simplices.
-S= PH_rips(X, 3, 0.3)
-
+radius = 0.1;
+S= PH_rips(X, 3, radius)
 %Display Ris complex
 PH_rips_display(X,S);
 %labels = cellstr(num2str((1:p)', '% d'));
 %text(X(:,1)+0.01, X(:,2)+0.01, X(:,3)+0.01, labels, 'Color', 'r', 'FontSize',16) 
 
-% -----------------
-%% Boundary matrices
+% Boundary matrices
 B = PH_boundary(S);
-betti = PH_betti(B)
+betti = PH_boundary_betti(B)
 title('\beta_0=1, \beta_1=4, \beta_2=0')
 
 
 % ---------------
-%% Sampling from a single Dirichlet distribution 
+%% Sampling persistent diagram
 
-alpha=[3,2,3]';
-weight=1;
+% Sampling from a single Dirichlet distribution 
+alpha=[3,2,3]'; %parameter
+weight=1;   %mixing propotion
 n=500;
 
-figure;
-Dirichelt_theoretical(alpha,weight);
-x = Dirichlet_sample(alpha,weight,n);
-hold on; scatter(x(:,1),x(:,2),100,'.k','MarkerFaceAlpha',0.05);
+x1 = Dirichlet_sample(alpha,weight,n);
+f1 = Dirichlet_density(alpha, weight, x1);
+figure; subplot(1,2,1); PH_PD_display(x1, f1); caxis([0 5])
 
-
-% ----------------
-%% Sampling from a mixture of Dirichlet distributions
-
-alpha=[3,8,2;8,3,2;7,3,8;3,7,8]';
-weight = [0.25,0.25,0.25,0.25];
+% Sampling from a 3-components mixture of Dirichlet distributions
+alpha=[3,8,2;8,3,2;7,3,8;3,7,8]'; %parameter
+weight = [0.25,0.25,0.25,0.25]; %mixing propotion
 n = 500;
 
-figure;
-Dirichelt_theoretical(alpha,weight);
-s = Dirichlet_sample(alpha,weight,n);
-hold on; scatter(s(:,1),s(:,2),100,'.k');
+x2 = Dirichlet_sample(alpha,weight,n);
+f2 = Dirichlet_density(alpha, weight, x2);
+subplot(1,2,2); PH_PD_display(x2, f2); caxis([0 5])
 
 
+%-------------------
+%% Graph filtrations
+% 
+% w = pdist2(X,X);
+% maxw = max(w(:));
+% 
+% thresholds=[0:0.01:maxw]; 
+% [beta0, beta1, biggest0, biggest1] = PH__betti(w, thresholds);
+% 
+% figure;
+% hold on; plot(thresholds,beta0, 'Color', 'y' , 'LineWidth',1,'LineStyle',':')
+% hold on; plot(thresholds,beta1, 'b', 'LineWidth',2,'LineStyle','-')
+% 
 
